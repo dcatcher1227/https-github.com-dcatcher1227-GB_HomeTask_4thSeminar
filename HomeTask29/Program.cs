39856ca4ee34b8cc883b171aa242e0c4426a8d8a
DC <d.catcher2014@yandex.ru>
2022-08-05 17:19:56 +0300

    Домашняя задача №29 к семенару 4

---
+++ HomeTask29/Program.cs
@@ -0,0 +1,10 @@
+﻿//  Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Random random = new Random();
+int size = 8;
+int[] myArr = new int[size];
+for (int i = 0; i < myArr.Length; i++)
+{
+    myArr[i] = random.Next(0, 99);
+    Console.Write(myArr[i] + " ");
+}
