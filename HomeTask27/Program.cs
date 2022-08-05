9b7c9f0798b1929410f162acfd0a153f5878f5a6
DC <d.catcher2014@yandex.ru>
2022-08-05 17:19:33 +0300

    Домашняя задача №27 к семенару 4

---
+++ HomeTask27/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+while (num != 0)
+{
+    sum += num % 10; 
+    num = num / 10;
+}
+Console.WriteLine(sum);
